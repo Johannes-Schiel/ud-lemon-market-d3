34e7d4828a7e5bcf1fbcbb077e9e40b03f751972
Johannes Schiel <j.schiel@unleashed-design.de>
2022-08-25 23:51:55 +0200

    11 Setup & Clear SVG D3

--- components/LemonMarketChartGraph.vue
+++ components/LemonMarketChartGraph.vue
@@ -13,8 +13,45 @@ export default {
 			required: true,
 		},
 	},
+	data() {
+		return {
+			width: 750,
+			height: 350,
+			margin: { top: 20, right: 20, bottom: 50, left: 50 },
+		};
+	},
+	computed: {
+		getSvg(): Element {
+			return document.querySelector('svg.graph');
+		},
+	},
+	methods: {
+		clearSvg(): void {
+			this.getSvg.innerHTML = '';
+		},
+		drawD3: function (): void {
+			this.clearSvg();
+			const svg = d3.select(this.getSvg);
+			svg.attr('viewBox', [0, 0, this.width, this.height]);
+		},
+	},
+	onUnmounted() {
+		window.removeEventListener('resize', this.drawD3);
+	},
+	mounted() {
+		this.drawD3();
+		window.addEventListener('resize', this.drawD3);
+	},
 	emits: ['userSelection'],
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+svg.graph {
+	width: 100%;
+	height: auto;
+	background-color: rgba($ciWhite, 0.05);
+	border-radius: 1rem;
+	color: rgba($ciWhite, 0.3);
+}
+</style>
