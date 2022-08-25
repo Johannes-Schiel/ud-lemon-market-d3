cf4202ca21cbb9a3563a5ff6064de45447175c70
Johannes Schiel <j.schiel@unleashed-design.de>
2022-08-25 23:11:22 +0200

    07 Request Instruments

--- app.vue
+++ app.vue
@@ -2,11 +2,29 @@
 	<div class="wrapper">
 		<LemonMarketLogo />
 		<LemonMarketSearch :init="route.query.search" />
+		<temaplte v-if="!pending && !error && instrument.results.length > 0">
+			{{ instrument }}
+		</temaplte>
+		<div class="error" v-if="error">Keine Daten gefunden</div>
 	</div>
 </template>
 
 <script lang="ts" setup>
 const route = useRoute();
+const config = useRuntimeConfig();
+
+const {
+	error,
+	pending,
+	data: instrument,
+} = await useFetch<InstrumentResponse>(
+	`https://data.lemon.markets/v1/instruments/?isin=${route.query.search}`,
+	{
+		headers: {
+			Authorization: `Bearer ${config.TOKEN}`,
+		},
+	}
+);
 </script>
 
 <style lang="scss">
