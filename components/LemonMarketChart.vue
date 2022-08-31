a3d5e1444fbb31c18f1212d53ad468e1c4d5a70c
Johannes Schiel <j.schiel@unleashed-design.de>
2022-08-31 09:48:24 +0200

    Update LemonMarketChart.vue

--- components/LemonMarketChart.vue
+++ components/LemonMarketChart.vue
@@ -17,8 +17,13 @@ const route = useRoute();
 const config = useRuntimeConfig();
 const state = reactive({ currentOhlc: null });
 
+const to = new Date();
+const from = new Date(new Date().setDate(to.getDate() - 55));
+
 const { data, pending, error } = await useFetch<OhlcResponse>(
-	`https://data.lemon.markets/v1/ohlc/d1?isin=${route.query.search}&from=2022-06-15T00:00:00.000Z&to=2022-07-30T00:00:00.000Z`,
+	`https://data.lemon.markets/v1/ohlc/d1?isin=${
+		route.query.search
+	}&from=${from.toISOString()}&to=${to.toISOString()}`,
 	{
 		headers: {
 			Authorization: `Bearer ${config.TOKEN}`,
