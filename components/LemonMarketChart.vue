5141152c329ea2bc7dbecfb2cd1f7533d4c00898
Johannes Schiel <j.schiel@unleashed-design.de>
2022-10-22 16:34:40 +0200

    Update NPM Packages

--- components/LemonMarketChart.vue
+++ components/LemonMarketChart.vue
@@ -1,5 +1,30 @@
-<template> </template>
+<template>
+	<template v-if="!pending && !error">
+		<LemonMarketChartOhlc
+			:ohlc="
+				state.currentOhlc || data.results[data.results.length - 1]
+			" />
+	</template>
+</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const route = useRoute();
+const config = useRuntimeConfig();
+const state = reactive({ currentOhlc: null });
+
+const to = new Date();
+const from = new Date(new Date().setDate(to.getDate() - 55));
+
+const { data, pending, error } = await useFetch<OhlcResponse>(
+	`https://data.lemon.markets/v1/ohlc/d1?isin=${
+		route.query.search
+	}&from=${from.toISOString()}&to=${to.toISOString()}`,
+	{
+		headers: {
+			Authorization: `Bearer ${config.TOKEN}`,
+		},
+	}
+);
+</script>
 
 <style lang="scss"></style>
